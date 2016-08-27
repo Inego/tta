18b70bcf1452483b7b26e44eae7e3df8d451e77b
Inego <yanis.batura@gmail.com>
2016-08-27 20:11:19 +0700

    Started leaders

---
+++ src/org/inego/tta2/cards/civil/leader/JuliusCaesar.java
@@ -0,0 +1,24 @@
+package org.inego.tta2.cards.civil.leader;
+
+import org.inego.tta2.gamestate.PlayerState;
+
+/**
+ * Created by Inego on 27.08.2016.
+ */
+public class JuliusCaesar extends LeaderCard {
+    @Override
+    public int getAge() {
+        return 0;
+    }
+
+    @Override
+    public String getName() {
+        return "Julius Caesar";
+    }
+
+
+    @Override
+    public void onElect(int sign, PlayerState playerState) {
+
+    }
+}
--- src/org/inego/tta2/cards/civil/leader/LeaderCard.java
+++ src/org/inego/tta2/cards/civil/leader/LeaderCard.java
@@ -2,6 +2,7 @@ package org.inego.tta2.cards.civil.leader;
 
 import org.inego.tta2.cards.civil.CivilCard;
 import org.inego.tta2.cards.civil.CivilCardKind;
+import org.inego.tta2.gamestate.PlayerState;
 
 /**
  * Created by Inego on 20.08.2016.
@@ -11,4 +12,11 @@ public abstract class LeaderCard extends CivilCard {
     public CivilCardKind getKind() {
         return CivilCardKind.LEADER;
     }
+
+    @Override
+    public int getQty(int numberOfPlayers) {
+        return 1;
+    }
+
+    public abstract void onElect(int sign, PlayerState playerState);
 }
