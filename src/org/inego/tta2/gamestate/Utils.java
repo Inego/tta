90810378a8c4bb0ce079e21d3cd0373fa00ab24d
Yanis Batura <yanis.batura@gmail.com>
2016-09-17 11:02:38 +0700

    Genghis Khan's formula for computing max armies + test

---
+++ src/org/inego/tta2/gamestate/Utils.java
@@ -0,0 +1,42 @@
+package org.inego.tta2.gamestate;
+
+/**
+ * Created by Inego on 17.09.2016.
+ */
+public class Utils {
+
+    /**
+     * Computes the maximum available armies consisting of required number of cavalry and infantry
+     * using Genghis Khan's special ability of considering infantry as cavalry if required.
+     *
+     * @param cavalry Number of cavalry units in the army, <strong>greater than 0</strong>
+     * @param infantry Number of infantry units in the army
+     * @param availableCavalry Available cavalry units
+     * @param availableInfantry Available infantry units
+     * @return Maximum number of armies
+     */
+    public static int getGenghisKhanMaxArmies(int cavalry, int infantry, int availableCavalry, int availableInfantry) {
+
+        /*
+        We must find the maximum i satisfying the following system of integer inequalities:
+        aC + d >= i * X
+        aI - d >= i * Y
+        d >= 0
+        i >= 0
+        where aC and aI are available cavalry and infantry, d is the number of infantry units considered as cavalry,
+        i is the number of armies, X and Y are cavalry and infantry composition of an army.
+
+        This can be rewritten as finding the biggest i such that
+        i <= (aC + aI) / (X + Y)
+        i <= aI / Y  --- since Y may be 0, this applies only when Y > 0
+        */
+
+        int result = (availableCavalry + availableInfantry) / (cavalry + infantry);
+
+        if (infantry > 0)
+            result = Math.min(result, availableInfantry / infantry);
+
+        return result;
+    }
+
+}
---
+++ tests/org/inego/tta2/gamestate/UtilsTest.java
@@ -0,0 +1,38 @@
+package org.inego.tta2.gamestate;
+
+import org.junit.Test;
+
+import static org.inego.tta2.gamestate.Utils.getGenghisKhanMaxArmies;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Inego on 17.09.2016.
+ */
+public class UtilsTest {
+
+    @Test
+    public void testGetGenghisKhanMaxArmies() throws Exception {
+
+        // Heavy cavalry
+        assertEquals(0, getGenghisKhanMaxArmies(3, 0, 1, 1));
+        assertEquals(1, getGenghisKhanMaxArmies(3, 0, 1, 2));
+        assertEquals(1, getGenghisKhanMaxArmies(3, 0, 3, 2));
+        assertEquals(2, getGenghisKhanMaxArmies(3, 0, 3, 3));
+
+        // Phalanx
+        assertEquals(0, getGenghisKhanMaxArmies(1, 2, 0, 2));
+        assertEquals(1, getGenghisKhanMaxArmies(1, 2, 1, 2));
+        assertEquals(1, getGenghisKhanMaxArmies(1, 2, 0, 3));
+        assertEquals(33, getGenghisKhanMaxArmies(1, 2, 0, 100));
+
+        // Conquistadors
+        assertEquals(0, getGenghisKhanMaxArmies(2, 1, 0, 2));
+        assertEquals(1, getGenghisKhanMaxArmies(2, 1, 1, 2));
+        assertEquals(1, getGenghisKhanMaxArmies(2, 1, 0, 5));
+        assertEquals(2, getGenghisKhanMaxArmies(2, 1, 0, 6));
+        assertEquals(10, getGenghisKhanMaxArmies(2, 1, 1000, 10));
+        assertEquals(336, getGenghisKhanMaxArmies(2, 1, 10, 1000));
+
+    }
+
+}
